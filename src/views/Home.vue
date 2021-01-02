6f4da9296d9a5d974f3b1c06e16bf45950f6f594
Sefa <60031458+fesa6654@users.noreply.github.com>
2021-01-02 13:57:01 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <main class="flexbox">
+      <Board id="board-1">
+        <Card id="card-1" draggable="true"><p>Card One</p></Card>
+        <Card id="card-4" draggable="true"><p>Card 4</p></Card>
+        <Card id="card-5" draggable="true"><p>Card 5</p></Card>
+        <Card id="card-6" draggable="true"><p>Card 6</p></Card>
+      </Board>
+
+      <Board id="board-2">
+        <Card id="card-2" draggable="true"><p>Card Two</p></Card>
+      </Board>
+    </main>
+  </div>
+</template>
+
+<script>
+import Board from "../components/Board";
+import Card from "../components/Card";
+
+export default {
+  components: {
+    Board,
+    Card,
+  },
+};
+</script>
+
+<style>
+.flexbox {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 768px;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0 auto;
+  padding: 15px;
+}
+
+.flexbox .board {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 300px;
+  background-color: black;
+  padding: 15px;
+}
+
+.flexbox .board .card {
+  padding: 15px 25px;
+  background-color: aliceblue;
+  cursor: pointer;
+  margin-bottom: 15px;
+}
+</style>
